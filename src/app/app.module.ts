11bcac82155a8af2bb0b74a1d37d86d7d13c334b
Witsanu Kumchalee <witsanu@cenergy.co.th>
2017-02-18 10:38:20 +0700

    Basic Ng2
    Basic Ng2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { HeroComponent } from './hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    HeroComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HeroComponent]
 })
 export class AppModule { }
---
+++ src/app/hero.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'hero',
+    template: `<h1>Progreanmer</h1>`
+})
+
+export class HeroComponent { }
+
